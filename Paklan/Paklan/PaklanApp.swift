d47fe10ad1832eea379473441b0839f18bd3a485
Felipe <felipeflima.2000@gmail.com>
2022-09-14 11:28:00 -0600

    Creation of project

---
+++ Paklan/Paklan/PaklanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaklanApp.swift
+//  Paklan
+//
+//  Created by Felipe Lima on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PaklanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
